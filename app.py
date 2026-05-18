03d157dd688dca1bf4d511a65e500fb2755f192c
alexanderpascual110 <24308060610056@cetis61.edu.mx>
2026-05-18 08:28:07 -0500

    cambiorealizados

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template, request, redirect, session
+from flask_mail import Mail, Message
+import random
 
 app = Flask(__name__)
 app.secret_key = "clave_secreta"
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = "TU_CORREO@gmail.com"
+app.config["MAIL_PASSWORD"] = "TU_PASSWORD_DE_APLICACION"
+
+mail = Mail(app)
+
+codigo_recuperacion = {}
 
 usuarios = []
 pacientes = []
@@ -49,6 +60,104 @@ def login():
         return "Correo o contraseña incorrectos"
 
     return render_template("login.html")
+
+# RECUPERAR CONTRASEÑA
+@app.route("/forgot", methods=["GET", "POST"])
+def forgot():
+
+    if request.method == "POST":
+
+        correo = request.form["correo"]
+
+        usuario = usuarios.find_one({
+            "correo": correo
+        })
+
+        if usuario:
+
+            codigo = str(random.randint(1000, 9999))
+
+            codigo_recuperacion[correo] = codigo
+
+            mensaje = Message(
+                "Recuperación de contraseña",
+                sender=app.config["MAIL_USERNAME"],
+                recipients=[correo]
+            )
+
+            mensaje.body = f"""
+Hola {usuario['nombre']}
+
+Tu código de recuperación es:
+
+{codigo}
+            """
+
+            mail.send(mensaje)
+
+            return redirect(f"/reset/{correo}")
+
+        return "Correo no encontrado"
+
+    return render_template("forgot.html")
+
+# NUEVA CONTRASEÑA
+@app.route("/reset/<correo>", methods=["GET", "POST"])
+def reset(correo):
+
+    if request.method == "POST":
+
+        codigo = request.form["codigo"]
+
+        nueva = request.form["password"]
+
+        if codigo_recuperacion.get(correo) == codigo:
+
+            usuarios.update_one(
+                {"correo": correo},
+                {
+                    "$set": {
+                        "password": nueva
+                    }
+                }
+            )
+
+            codigo_recuperacion.pop(correo)
+
+            return redirect("/login")
+
+        return "Código incorrecto"
+
+    return render_template("reset.html", correo=correo)
+
+# NUEVA CONTRASEÑA
+@app.route("/reset/<correo>", methods=["GET", "POST"])
+def reset_password(correo):
+
+    if request.method == "POST":
+
+        codigo = request.form["codigo"]
+
+        nueva = request.form["password"]
+
+        if codigo_recuperacion.get(correo) == codigo:
+
+            usuarios.update_one(
+                {"correo": correo},
+                {
+                    "$set": {
+                        "password": nueva
+                    }
+                }
+            )
+
+            codigo_recuperacion.pop(correo)
+
+            return redirect("/login")
+
+        return "Código incorrecto"
+
+    return render_template("reset.html", correo=correo)
 
 # DASHBOARD
 @app.route("/dashboard")
